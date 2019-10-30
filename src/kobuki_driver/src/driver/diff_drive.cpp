33e7b85a83fe9086d9e55dd42da6352133d8f5bd
zouwubing <1756681646@qq.com>
2019-10-30 11:53:23 +0800

    add krb100m parameter

--- src/kobuki_driver/src/driver/diff_drive.cpp
+++ src/kobuki_driver/src/driver/diff_drive.cpp
@@ -32,9 +32,11 @@ DiffDrive::DiffDrive() :
 //  v(0.0), w(0.0), // command velocities, in [m/s] and [rad/s]
   radius(0.0), speed(0.0), // command velocities, in [mm] and [mm/s]
   point_velocity(2,0.0), // command velocities, in [m/s] and [rad/s]
-  bias(0.223), // wheelbase, wheel_to_wheel, in [m]
+  bias(0.161), // wheelbase, wheel_to_wheel, in [m]
+  //bias(0.223), // wheelbase, wheel_to_wheel, in [m]
   wheel_radius(0.032), // radius of main wheel, in [m]
-  tick_to_rad(0.00317332585858586),
+  //tick_to_rad(0.00317332585858586),
+  tick_to_rad(0.004197185),
   diff_drive_kinematics(bias, wheel_radius)
 {}
 
